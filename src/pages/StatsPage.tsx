c3b139bcb670088c7e2ed452ce2a5db0b4c62673
Preston Santiago <prestongsantiago@gmail.com>
2024-06-28 00:17:54 -0400

    Combined OpenStatsRow and ClosedStatsRow into one StatsRow component. Updated statsdata to show time as completionTime to avoid confusion in naming

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -1,7 +1,5 @@
 import NavBar from "../components/NavBar";
-import OpenArrow from "../assets/icons/Open-Arrow.svg";
-import ClosedStatsRow from "../components/ClosedStatsRow";
-import OpenStatsRow from "../components/OpenStatsRow";
+import StatsRow from "../components/StatsRow";
 
 const fakeStatsData = [
   {
@@ -9,9 +7,12 @@ const fakeStatsData = [
     avg: "00:00:000",
     best: "11:11:111",
     times: [
-      { dateTime: new Date().toLocaleTimeString(), time: "22:22:222" },
-      { dateTime: new Date().toLocaleTimeString(), time: "33:33:333" },
-      { dateTime: new Date().toLocaleTimeString(), time: "44:44:444" },
+      {
+        dateTime: new Date().toLocaleTimeString(),
+        completionTime: "22:22:222",
+      },
+      { dateTime: "13:05:01 AM", completionTime: "33:33:333" },
+      { dateTime: "14:05:01 AM", completionTime: "44:44:444" },
     ],
   },
 ];
@@ -24,29 +25,8 @@ export default function StatsPage() {
         Statistics
       </h1>
       <ul>
-        <ClosedStatsRow fakeStatsData={fakeStatsData} />
-        <li className="bg-primary-light border-b-2 border-black">
-          <div className="flex flex-row items-center mb-4">
-            <button>
-              <img className="m-6" src={OpenArrow} alt="Right facing arrow" />
-            </button>
-            <p className="text-3xl">All Times: {fakeStatsData[0].date}</p>
-          </div>
-          <ul className="grid grid-cols-2 text-center">
-            <li className="text-2xl mb-4">
-              {fakeStatsData[0].times[0].dateTime}
-            </li>
-            <li className="text-2xl mb-4">{fakeStatsData[0].times[0].time}</li>
-            <li className="text-2xl mb-4">
-              {fakeStatsData[0].times[1].dateTime}
-            </li>
-            <li className="text-2xl mb-4">{fakeStatsData[0].times[1].time}</li>
-            <li className="text-2xl mb-4">
-              {fakeStatsData[0].times[2].dateTime}
-            </li>
-            <li className="text-2xl mb-4">{fakeStatsData[0].times[2].time}</li>
-          </ul>
-        </li>
+        <StatsRow statsData={fakeStatsData[0]} />
+        <StatsRow statsData={fakeStatsData[0]} />
       </ul>
     </>
   );
